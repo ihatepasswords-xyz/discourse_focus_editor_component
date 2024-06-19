400519590c0e5494eca0e479c9cd62446d1f23d8
eegrok <304507+eegrok@users.noreply.github.com>
2024-06-20 09:32:23 +1200

    add js code for component

--- javascripts/discourse/api-initializers/focus_editor_if_title_specified.js
+++ javascripts/discourse/api-initializers/focus_editor_if_title_specified.js
@@ -1,5 +1,12 @@
 import { apiInitializer } from "discourse/lib/api";
+import putCursorAtEnd from "discourse/lib/put-cursor-at-end";
 
-export default apiInitializer("1.8.0", (/* api */) => {
-  // console.log("hello world from api initializer!");
+export default apiInitializer("1.8.0", (api) => {
+  api.onAppEvent("composer:open", ({ model }) => {
+    if (model.title !== "") {
+      scheduleOnce("afterRender", () => {
+        putCursorAtEnd(document.querySelector("textarea.d-editor-input"));
+      });
+    }
+  });
 });
