98c839868b536a707f9094e986e60781eda04ab8
eegrok <304507+eegrok@users.noreply.github.com>
2024-06-20 09:36:57 +1200

    add missing import

--- javascripts/discourse/api-initializers/focus_editor_if_title_specified.js
+++ javascripts/discourse/api-initializers/focus_editor_if_title_specified.js
@@ -1,5 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 import putCursorAtEnd from "discourse/lib/put-cursor-at-end";
+import { scheduleOnce } from "@ember/runloop";
 
 export default apiInitializer("1.8.0", (api) => {
   api.onAppEvent("composer:open", ({ model }) => {
